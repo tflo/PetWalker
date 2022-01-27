6c97f0db631cfe9dc766f046f9238569b4aeba46
Tom Floeren <thfl@mac.com>
2022-01-27 12:43:47 +0100

    Messages

--- PetWalker.lua
+++ PetWalker.lua
@@ -153,7 +153,7 @@ Messages
 
 -- TODO: Do we need a warning at 1 selectable pet? Or should this be considered a valid use-case? (User manually summons a pet from Journal, but wants to get back his (only) fav pet when the timer is due.)
 local function MsgLowPetPool(nPool)
-	DEFAULT_CHAT_FRAME:AddMessage(addonName .. ": " .. (nPool < 1 and "0 (zero) pets" or "Only 1 pet") .. " eligible for summoning! You should either " .. (ns.db.favsOnly and "flag more pets as favorite, or set the ramdom pool to 'All Pets'" or "collect more pets") .. ", or set the random-summon timer to '0'. Please note that certain pets are excluded from random summoning, to not break their usability (for example Guild Herald)." .. ((ns.dbc.charFavsEnabled and ns.db.favsOnly) and "\nNote that you have set this char to use char-specific favorite pets. Maybe switching to global favorites ('/pw c') will help."),0,1,0.7)
+	DEFAULT_CHAT_FRAME:AddMessage(addonName .. ": " .. (nPool < 1 and "0 (zero) pets" or "Only 1 pet") .. " eligible for summoning! You should either " .. (ns.db.favsOnly and "flag more pets as favorite, or set the ramdom pool to 'All Pets'" or "collect more pets") .. ", or set the random-summon timer to '0'. Please note that certain pets are excluded from random summoning, to not break their usability (for example Guild Herald)." .. ((ns.dbc.charFavsEnabled and ns.db.favsOnly) and "\nNote that you have set this char to use char-specific favorite pets. Maybe switching to global favorites ('/pw c') will help." or ""),0,1,0.7)
 end
 
 local function MsgNoSavedPet()
@@ -161,7 +161,7 @@ local function MsgNoSavedPet()
 end
 
 local function MsgAutoRestoreDone(pet)
-	DEFAULT_CHAT_FRAME:AddMessage(addonName .. ": Your last pet (" .. ns.PetIDtoName(pet) .. ") has been re-summoned.", 0,1,0.7)
+	DEFAULT_CHAT_FRAME:AddMessage(addonName .. ": Restored your last pet (" .. ns.PetIDtoName(pet) .. ")", 0,1,0.7)
 end
 
 local function MsgNewPetDone(pet, nPool)
