ff871d84396139d418708626031f28ef10cfa8f2
Tom <tflo@users.noreply.github.com>
2023-04-26 14:11:13 +0200

    Cmd feedback: enabling favs tells you if char favs are enabled on this char

--- ui.lua
+++ ui.lua
@@ -351,7 +351,7 @@ function ns:FavsToggle()
 	ns.db.favsOnly = not ns.db.favsOnly
 	ns.poolInitialized, ns.petVerified = false, false
 	if ns.db.autoEnabled then ns:NewPet() end
-	ChatUserNotification(format('%sSelection pool: %s.', CO.bn, (ns.db.favsOnly and 'favorites only' or 'all pets')))
+	ChatUserNotification(format('%sPet pool: %s%s.', CO.bn, ns.db.favsOnly and 'favorites only' or 'all pets', ns.db.favsOnly and ns.dbc.charFavsEnabled and ' (char-specific)' or ns.db.favsOnly and ' (global)' or ''))
 end
 
 function ns.CharFavsSlashToggle() -- for slash command only
